bb46726b91087b0f523cfab1753d0ccfd947ba3a
Robbie Mackay <rm@robbiemackay.com>
2012-08-23 14:40:06 +1200

    Handle default sharing filter config in sharing bar.
    Doesn't work oninitial load yet

--- views/sharing/sharing_bar.php
+++ views/sharing/sharing_bar.php
@@ -6,11 +6,11 @@
 	</strong>
 </div>
 		<ul id="sharing_switch" class="category-filters">
-			<li><a href="#" id="share_all" class="active">
+			<li><a href="#" id="share_all" <?php if (Kohana::config('sharing_two.default_sharing_filter') == 'all') echo' class="active"'; ?>>
 				<div class="swatch" style="background-color:#<?php echo Kohana::config('settings.default_map_all'); ?>"></div>
 				<div><?php echo Kohana::lang('sharing_two.all_sites') ?></div>
 			</a></li>
-			<li><a href="#" id="share_main">
+			<li><a href="#" id="share_main"<?php if (Kohana::config('sharing_two.default_sharing_filter') == 'main') echo' class="active"'; ?>>
 				<div class="swatch" style="background-color:#<?php echo Kohana::config('settings.default_map_all'); ?>"></div>
 				<div><?php echo Kohana::config('settings.site_name') ?></div>
 			</a></li>
@@ -20,7 +20,8 @@
 			{
 				$sharing_name = $share_info[0];
 				$sharing_color = $share_info[1];
-				echo '<li><a href="#" id="share_'. $share .'"><div class="swatch" style="background-color:#'.$sharing_color.'"></div>
+				$class = (Kohana::config('sharing_two.default_sharing_filter') == $share) ? "active" : '';
+				echo '<li><a href="#" id="share_'. $share .'" class="'.$class.'"><div class="swatch" style="background-color:#'.$sharing_color.'"></div>
 				<div>'.$sharing_name.'</div></a></li>';
 			}
 			?>
